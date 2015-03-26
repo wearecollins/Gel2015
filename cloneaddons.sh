c9277a16a3b65a8f729f9ae0e5479e682175f76e
Brett Renfer <brett@robotconscience.com>
2015-03-26 11:02:16 -0400

    Renaming p_Bar since it's become working output app, added cloneaddons.sh

---
+++ cloneaddons.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#make sure you are in the Gel folder when you run this.
+
+cd ../../addons
+
+
+PREFIX="git clone https://github.com/"
+
+# gui
+${PREFIX}rezaali/ofxUI.git
+
+# comms
+${PREFIX}labatrockwell/ofxLibwebsockets.git
+${PREFIX}Spacebrew/ofxSpacebrew.git
+
+# ui
+${PREFIX}robotconscience/ofxCocoa.git --branch dev
