235e4a176095a4d93fb634e403c8afe27fd7f7d0
Brett Renfer <brett@robotconscience.com>
2015-04-19 16:59:37 -0400

    Badass 300 connection tester

--- p_RandomSender/src/ofApp.cpp
+++ p_RandomSender/src/ofApp.cpp
@@ -8,89 +8,108 @@ ofVec2f p,t;
 int last = 0;
 int rate = 500;
 
+int number = 300;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-    id = ofToString(ofRandom(10000),0);
+    times.resize(number);
+    values.resize(number);
     
-    spacebrew.addPublish("touch", "gelpoint");
+    for ( int i=0; i<number; i++){
+        spacebrew.push_back(new Spacebrew::Connection());
+        id = ofToString(ofRandom(10000),0);
+        
+        values[i] = (ofRandom(2));
+        times[i] = ofRandom(10000);
+        
+        spacebrew.back()->addPublish("direction", "pad");
+        spacebrew.back()->connect( "127.0.0.1", "randomsender_"+ id);
+    }
     
-    spacebrew.connect( "spacebrew.robotconscience.com", "randomsender_"+ id);
     ofSetFrameRate(30);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    
-    if (ofGetElapsedTimeMillis() - last > rate ){
-        rate = ofRandom(5000);
-        last = ofGetElapsedTimeMillis();
-        
-        t.set( ofRandom(-1,1),0);
-        cout << "set "<<t<<endl;
+    for (int i=0; i<times.size(); i++){
+        if ( ofGetElapsedTimeMillis() - times[i] > rate){
+            times[i] = ofGetElapsedTimeMillis();
+            values[i] = round(ofRandom(2));
+            
+            spacebrew[i]->send("direction", "pad", "\""+ofToString(values[i]) +":"+spacebrew[i]->getConfig()->clientName+":0\"");
+        }
     }
     
-    p = p * ofVec2f(.7,.7) + t * ofVec2f(.3,.3`);
-    
-    stringstream ss;
-    ss<<"{\"x\":\""<<ofToString(p.x) << "\",";
-    ss<<"\"y\":\"" << ofToString(p.y) << "\",";
-    ss<<"\"id\":\"" << id << "\",";
-        ss<<"\"color\":{";
-            ss<<"\"r\":" << (int) (color.r) << ",";
-            ss<<"\"g\":" << (int) (color.g) << ",";
-            ss<<"\"b\":" << (int) (color.b) << "";
-        ss<<"}";
-    ss<<"}";
-    
-    spacebrew.send("touch", "gelpoint", ss.str());
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    float x =0;
+    float y = 20;
+    float w = ofGetWidth() / 15.;
+    float h = (ofGetHeight()-20) / 20.;
+    ofNoFill();
+    
+    float t = 0;
+    
+    for ( auto & v : values ){
+        ofRect(x, y, w, h);
+        ofDrawBitmapString(ofToString(v), ofPoint(x + 20, y + 20));
+        x += w;
+        if ( x + w > ofGetWidth() ){
+            x = 0;
+            y += h;
+        }
+        
+        t += v;
+    }
+    
+    t /= values.size();
+    
+    ofDrawBitmapString("AVERAGE :"+ofToString(t), 0,20);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
--- p_RandomSender/src/ofApp.h
+++ p_RandomSender/src/ofApp.h
@@ -4,21 +4,23 @@
 #include "ofxSpacebrew.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
-        Spacebrew::Connection spacebrew;
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    vector<Spacebrew::Connection *> spacebrew;
+    vector<int> values;
+    vector<int> times;
 };
